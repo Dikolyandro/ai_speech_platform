567eeccd711c6aebcf7247acf4fbc1065cef95ca
Dikolyandro <adil.baltabayev123@gmail.com>
2026-06-09 03:21:11 +0500

    Add SSL support for Aiven MySQL

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,12 +1,20 @@
+import ssl
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from app.core.config import settings
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 
+connect_args = {}
+
+if settings.DATABASE_URL.startswith("mysql+aiomysql"):
+    ssl_context = ssl.create_default_context()
+    connect_args["ssl"] = ssl_context
+
 engine = create_async_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True,
+    connect_args=connect_args,
 )
 
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
@@ -15,4 +23,3 @@ Base = declarative_base()
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
-        
